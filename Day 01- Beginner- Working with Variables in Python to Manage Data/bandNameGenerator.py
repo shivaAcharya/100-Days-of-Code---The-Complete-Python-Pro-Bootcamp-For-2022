0512a3e524a3156fed1f61a311ff7fbf133c8253
Shiva Acharya <cva.acharya8@gmail.com>
2022-01-09 16:02:57 -0800

    Add files via upload

---
+++ Day 01- Beginner- Working with Variables in Python to Manage Data/bandNameGenerator.py
@@ -0,0 +1,14 @@
+#1. Create a greeting for your program.
+print("Hello, there")
+
+#2. Ask the user for the city that they grew up in.
+city = input("What city did you grow up in?\n")
+
+#3. Ask the user for the name of a pet.
+pet_name  = input("What is the name of your pet?\n")
+
+#4. Combine the name of their city and pet and show them their band name.
+print(f"Your band name is {city + pet_name}")
+
+#5. Make sure the input cursor shows on a new line, see the example at:
+#   https://replit.com/@appbrewery/band-name-generator-end
